5a952eebb4573815421ed9579a27a64928346d2c
m1nhblu <93319866+m1nhblu@users.noreply.github.com>
2024-11-23 15:07:02 +0700

    Add files via upload

---
+++ inc/arrays/arrays.php
@@ -0,0 +1,40 @@
+<?php
+$navbarItems = [
+  'Shop' => 'index.php',
+  'FAQ' => 'faq.php',
+  'Tracking' => 'tracking.php',
+  'Contact Us' => 'contact.php'
+];
+$faqItems = [
+  [
+    'question' => 'Bạn có hỗ trợ đổi trả hàng không?',
+    'answer' => 'Chúng tôi không hỗ trợ đổi trả hàng vì đây là sản phẩm digital.'
+  ],
+  [
+    'question' => 'Bạn có hỗ trợ sản phẩm không?',
+    'answer' => 'Chúng tôi cung cấp hỗ trợ đầy đủ cho sản phẩm của chúng tôi. Nếu bạn có bất kỳ câu hỏi hoặc vấn đề nào, bạn có thể liên hệ với đội ngũ hỗ trợ của chúng tôi qua email hoặc thông qua cổng khách hàng của chúng tôi.'
+  ],
+  [
+    'question' => 'Sản phẩm của bạn có bảo hành không?',
+    'answer' => 'Đúng, mỗi sản phẩm của chúng tôi đi kèm với bảo hành riêng. Chi tiết về bảo hành có thể được tìm thấy trong tài liệu sản phẩm hoặc trên trang sản phẩm.'
+  ],
+  [
+    'question' => 'Bạn chấp nhận phương thức thanh toán nào?',
+    'answer' => 'Chúng tôi chấp nhận nhiều phương thức thanh toán khác nhau bao gồm tiền mặt, thẻ tín dụng và chuyển khoản ngân hàng. Bạn có thể chọn phương thức phù hợp với bạn nhất khi mua hàng.'
+  ],
+  [
+    'question' => 'Bạn có hỗ trợ thanh toán khi nhận hàng không?',
+    'answer' => 'Đúng, chúng tôi cung cấp dịch vụ thanh toán khi nhận hàng cho khách hàng. Bạn có thể nhận hàng và thanh toán cho sản phẩm tại địa chỉ được chỉ định.'
+  ],
+  [
+    'question' => 'Bạn cung cấp những phương thức thanh toán nào cho khách hàng?',
+    'answer' => 'Chúng tôi cung cấp nhiều phương thức thanh toán tiện lợi cho khách hàng, bao gồm thanh toán bằng tiền mặt, thẻ tín dụng, chuyển khoản ngân hàng và PayPal. Bạn có thể chọn phương thức phù hợp với sở thích và nhu cầu của bạn.'
+  ],
+  [
+    'question' => 'Bạn có chấp nhận thanh toán bằng thẻ tín dụng không?',
+    'answer' => 'Đúng, bạn có thể sử dụng thẻ tín dụng của bạn để thực hiện thanh toán. Chúng tôi chấp nhận các thẻ tín dụng phổ biến như Visa, MasterCard, American Express và nhiều hơn nữa.'
+  ],
+
+
+];
+
---
+++ inc/languages/vi.php
@@ -0,0 +1,17 @@
+<?php
+$lang = [
+  'Devine Store' => 'Devine Store',
+  'Ltr' => 'ltr',
+  'En' => 'vi',
+  'Bootstrap' => 'bootstrap.min.css',
+  'English' => 'Tiếng Anh',
+  'VietNam' => 'Tiếng Việt',
+  'Shop' => 'Cửa Hàng',
+  'FAQ' => 'FAQ',
+  'Tracking' => 'Theo Dõi',
+  'Contact Us' => 'Liên Hệ',
+  'Search' => 'Tìm Kiếm',
+  'Devine Store online' => 'Devine Store online',
+  'Last Products' => 'Sản Phẩm Mới',
+  'Cart' => 'Giỏ Hàng'
+];
